3eea0afb50e21aca7ac2cdb0504804e1adbea0c5
Maxime Denoun <maxime_denoun@hotmail.com>
2020-12-06 20:10:46 +0200

    improved

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {WeatherService} from './weather.service';
 import {Observable} from 'rxjs';
-
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
